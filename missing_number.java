da7dc5529f4b484128d9a998a9e0178c469d2465
Nirbhya Kaushik <53752594+Nirbhya@users.noreply.github.com>
2020-06-19 16:15:00 +0530

    Add files via upload
    Program to find the missing number from 1 to 100 or (1 to n) from an Array.

---
+++ missing_number.java
@@ -0,0 +1,21 @@
+// WAP to find the missing number is a array.
+// Program is written by Nirbhay Kaushik
+
+class missing_number
+{
+  public static void main(String[] args)
+  {
+    // int[] arr=new int[]{1,2,4,3,6,5,8,10,11,7,12,9};
+    // missing number is 9.
+    int[] arr=new int[]{1,2,4,3,6,5,8,10,11,7,12};
+    int sum=0,total=0;
+    // n*(n+1)/2
+    total=((arr.length+1)*(arr.length+1+1))/2;
+    
+    for(int i=0;i<arr.length;i++)
+    {
+      sum+=arr[i];
+    }
+    System.out.println(total-sum);
+  }
+}
